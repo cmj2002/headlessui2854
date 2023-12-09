9fd4472a86c498bd0dcd1f7adbe8859b47e90568
Cao Mingjun <me@caomingjun.com>
2023-12-10 00:12:06 +0800

    Add minimal reproduction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { Menu } from "@headlessui/react";
 
 export default function Home() {
   return (
@@ -27,6 +28,35 @@ export default function Home() {
           </a>
         </div>
       </div>
+
+      <Menu>
+        <Menu.Button>More</Menu.Button>
+        <Menu.Items>
+          <Menu.Item>
+            {({ active }) => (
+              <a
+                className={`${active && 'bg-blue-500'}`}
+                href="/account-settings"
+              >
+                Account settings
+              </a>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <a
+                className={`${active && 'bg-blue-500'}`}
+                href="/account-settings"
+              >
+                Documentation
+              </a>
+            )}
+          </Menu.Item>
+          <Menu.Item disabled>
+            <span className="opacity-75">Invite a friend (coming soon!)</span>
+          </Menu.Item>
+        </Menu.Items>
+      </Menu>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
